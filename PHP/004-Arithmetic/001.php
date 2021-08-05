de27c5878a78d0983fa21077ba672e5c99b6d734
krisamorim <kris.a.furtado@gmail.com>
2021-08-05 01:30:26 +0000

    Php unary, logical operator

--- PHP/004-Arithmetic/001.php
+++ PHP/004-Arithmetic/001.php
@@ -14,6 +14,11 @@
         echo "1st way: The sum is $sum </br>";
         echo "2nd way: The sum is ".($n1+$n2);
         echo "<br> <br> *Check code to compare";
+
+        echo "<br> <br>";
+        echo "n2=$n2 <br>increment is ".++$n2 . "<br>Now n2 is $n2 <br> decrement is ".--$n2;
+        
+
     ?>
 </body>
 </html>
