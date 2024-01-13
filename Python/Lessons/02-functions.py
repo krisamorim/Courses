f8e5b0b730db58bab786e1ec861c050ad4b64e61
krisamorim <38765719+krisamorim@users.noreply.github.com>
2024-01-13 19:34:32 +0000

    lesson 02 functions

---
+++ Python/Lessons/02-functions.py
@@ -0,0 +1,4 @@
+#doing root square and round up
+from math import sqrt, ceil
+number01 = int(input("Enter a number: "))
+print(" The square root of {} is {}\nRound up is {}".format(number01, sqrt(number01), ceil(sqrt(number01))))
