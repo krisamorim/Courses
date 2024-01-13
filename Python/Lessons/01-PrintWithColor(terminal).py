f15564091f95e97c2daca531f4c0477763a7cda0
krisamorim <38765719+krisamorim@users.noreply.github.com>
2024-01-13 19:50:04 +0000

    color in terminal

---
+++ Python/Lessons/01-PrintWithColor(terminal).py
@@ -0,0 +1,17 @@
+print('\033[1;31;43mRed in yellow BOLD\033[m')
+print('\033[4;0;45mWhite in pink UNDERLINE\033[m')
+print('\033[0;30;41mWhite in red\033[m')
+print('\033[4;33;44mYellow in blue UNDERLINE\033[m')
+print('\033[1;35;43mPurple in yellow BOLD\033[m')
+print('\033[;42mBlack in gree\033[m')
+print('\033[mGray in black\033[m')
+print('\033[7;30mBlack in gray\033[m')
+
+#0 none
+#1 bold
+#4 underline
+#7 negativo
+
+#cor: 30-Black 31-Red 32-Green 33-Yellow 34-Blue 35-Purple 36-LightBlue 37-Gray
+
+#background de 40 a 47
