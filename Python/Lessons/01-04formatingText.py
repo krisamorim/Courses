7b128538b7df3ae799b32c2c6890ec265a412580
krisamorim <38765719+krisamorim@users.noreply.github.com>
2024-01-13 19:55:56 +0000

    remaned files

---
+++ Python/Lessons/01-04formatingText.py
@@ -0,0 +1,8 @@
+equals = "="
+print(40*equals,'\nFormatando texto')
+print('_'*40)
+
+a = 'X'
+print('Center\n{:=^20}!'.format(a))
+print('To left\n{:=<20}!'.format(a))
+print('To right\n{:=>20}!'.format(a))
