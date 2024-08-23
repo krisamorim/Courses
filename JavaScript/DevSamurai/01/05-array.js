0dfb2321718aeb178d7cffa3fc445e030bcbdab5
kris.amorim <kris.a.furtado@gmail.com>
2024-08-23 20:58:14 -0300

    array

---
+++ JavaScript/DevSamurai/01/05-array.js
@@ -0,0 +1,12 @@
+
+var nomes = ["Jesus","kris", "ana", "samuel","others"];
+var umArrayVazio = [];
+console.log("The array length is: "+nomes.length+"\nThe element from position zero is: "+nomes[0]);
+
+console.log("\nA empty array: "+umArrayVazio);
+
+console.log("\nArray nome = "+nomes+"\nAdd God in 1st position the array length change to "+nomes.unshift("God")+"\nThe updated array is = "+nomes);
+
+console.log("\nIf remove "+nomes.pop()+" the new array is: "+ nomes); //remove last element
+
+console.log("\nThe position of the ana element is: "+nomes.indexOf("ana")); //show position
