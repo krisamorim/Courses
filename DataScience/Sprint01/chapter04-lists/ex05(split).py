7ab37c3230567f9f85749d7ab1f5ed5b0fb95da4
krisamorim <kris.a.afurtado@gmail.com>
2026-03-19 09:09:22 -0300

    nestedLis, split e join

---
+++ DataScience/Sprint01/chapter04-lists/ex05(split).py
@@ -0,0 +1,15 @@
+phrase = 'vamos dividir ou não vamos dividir'
+words = phrase.split()
+print(f'The value of the variable phrase is: {phrase} and the type of the variable is: {type(phrase)}')
+print(f'The value of the variable words is: {words} and the type of the variable is: {type(words)}')
+print(f'The length of the phrase is: {len(phrase)} anda the length of the list is: {len(words)}')
+
+
+#======================================= Another example =======================================
+
+phrase = 'Brilha-brilha-estrelinha-lá-no-céu-pequenininha'
+words = phrase.split('-')
+print(f'\nVariable phrase is: {phrase} ')
+print(f'Variable words is: {words}')
+
+
---
+++ DataScience/Sprint01/chapter04-lists/ex06(join).py
@@ -0,0 +1,3 @@
+heroes = ['Homem-Aranha', 'Batman', 'Super-Homem', 'Homem de Ferro']
+comparison = '>'.join(heroes)
+print(comparison) 
